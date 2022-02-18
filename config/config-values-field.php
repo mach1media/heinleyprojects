cc91372836f9cb9a5668efae424025f23e97ce64
Roger Glenn <roger@mach1media.com>
2022-02-18 11:09:11 -0600

    installed Craft CMS. front-end change requests.

---
+++ config/config-values-field.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Color Values for dropdown
+ */
+
+return [
+	'data' => [
+		
+		'CSS Color Classes' => [
+			'default'         => 'Default',
+			'red'             => 'Red',
+			'white'           => 'White',
+			'silver'          => 'Silver',
+			'gray'            => 'Gray',
+			'gray-light'      => 'Gray: Light',
+			'gray-dark'       => 'Gray: Dark',
+			'black'           => 'Black',
+		],
+		
+	]
+];
